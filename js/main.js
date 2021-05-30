bfab5309dd0923b45162eedff4ab71ff15c8c503
Vadim-Kliui <67415419+Vadim-Kliui@users.noreply.github.com>
2021-05-30 20:28:17 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -62,7 +62,7 @@ function initialize() {
 // load google map
 var script = document.createElement('script');
     script.type = 'text/javascript';
-    script.src='https://maps.googleapis.com/maps/api/js?key=AIzaSyAZdlwoxgra6lFbClGhHkHXQuWyvPW61fg&sensor=false&' +
+    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
         'callback=initialize';
     document.body.appendChild(script);
 	 
